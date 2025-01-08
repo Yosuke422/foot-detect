3198c26dae7808e8c6b258406e0b6fbc1ef2d7a1
youssef Charafeddine <yosuke2017@hotmail.com>
2025-01-08 09:11:48 +0100

    Add Navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* Rest of your page content here */}
+        <h1 style={{ marginTop: '100vh', textAlign: 'center' }}>Scroll Down!</h1>
+    </div>
+  );
+}
+
+export default App;
+
