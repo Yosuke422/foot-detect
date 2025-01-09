d0e53df1fb5a4c0ce3e905751a53f6a2dce28acb
mrazakandraina <mrazakandraina@optilian.com>
2025-01-09 14:02:38 +0100

    fix : régler le problème de connexion

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { useAuth } from "../components/AuthContext"
-import "../assets/styles/Login.css"
-import stade from "../assets/images/stade.jpg"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../components/AuthContext";
+import { loginUser } from "../services/authService";
+import "../assets/styles/Login.css";
+import stade from "../assets/images/stade.jpg";
 
 const Login = () => {
-  const [formData, setFormData] = useState({ email: "", password: "" })
-  const [error, setError] = useState("")
-  const { login } = useAuth()
-  const navigate = useNavigate()
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
+  const { login } = useAuth(); // Hook AuthContext
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-  }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    setError("")
+    e.preventDefault();
+    setError("");
 
     try {
-      const fakeToken = "fake-jwt-token"
-      login(fakeToken) 
-      navigate("/") 
+      const { token } = await loginUser(formData); // Valide les credentials
+      login(token); // Appelle le login de AuthContext
+      navigate("/"); // Redirige vers la page d'accueil
     } catch (err) {
-      setError("Connexion échouée")
+      setError(err.message); // Affiche l'erreur renvoyée par loginUser
     }
-  }
+  };
 
   return (
     <div className="login-container" style={{ backgroundImage: `url(${stade})` }}>
@@ -59,12 +60,14 @@ const Login = () => {
           <button type="submit" className="login-button">
             Se connecter
           </button>
-          <p> Vous n'avez pas de compte ? <a href="/register">Inscrivez-vous</a></p>
+          <p>
+            Vous n'avez pas de compte ? <a href="/register">Inscrivez-vous</a>
+          </p>
         </form>
         {error && <p style={{ color: "red" }}>{error}</p>}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
